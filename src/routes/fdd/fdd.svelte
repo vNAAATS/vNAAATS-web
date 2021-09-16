5084a82a63d79f86834cc6ae9c662eb000cdd9d2
Andrew Ogden <andrewogden@bigpond.com>
2021-09-17 05:05:03 +1000

    screwed up, put relevance code back

--- src/routes/fdd/fdd.svelte
+++ src/routes/fdd/fdd.svelte
@@ -133,7 +133,7 @@
           </div>
         {/if}
         {#each Array.from(dataHandler.networkAircraft.get(acTrack).values()) as ac}
-        {#if ac.Relevant }
+        {#if ac.Relevant && new Date().getUTCMinutes() - new Date(ac.LastUpdated).getUTCMinutes() < 5}
           <FlightStrip 
             callsign={ac.Callsign}
             type={ac.Type}
